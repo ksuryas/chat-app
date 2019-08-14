c2dbf2cc0d277497f98cfb8ede8c065aae99452c
Surya <surya.kasibhatla@gmail.com>
2019-08-14 18:40:55 -0500

    For fixing bug

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,50 @@
-const socket = io();
+const socket = io(); 
 const log = console.log;
+
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $shareLocation = document.querySelector('#shareLocation');
+
+const $messages = document.querySelector('#messages');
+const messageTemplate = document.querySelector('#message-template');
 
 socket.on('message', (msg) => {
 	log(msg);
+	const html = Mustache.render(messageTemplate);
+	log(typeof(html));
+	$messages.insertAdjacentElement('beforeend', html);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+    $messageFormButton.setAttribute('disabled', 'disabled');
 	const message = document.querySelector('input').value;
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		$messageFormButton.removeAttribute('disabled');
+		$messageFormInput.value = '';
+		$messageFormInput.focus();
+		
+		if(error) {
+			return log(error);
+		}
+		log('Message delivered');
+	});
+});
+$shareLocation.addEventListener('click', () => {
+	if(!navigator.geolocation) {
+		return alert('Geolocation not supported for your browser'); 
+	}
+	
+	$shareLocation.setAttribute('disabled', 'disabled');
+	navigator.geolocation.getCurrentPosition((position) => {
+		socket.emit('sendLocation', {
+			lat: position.coords.latitude,
+			long: position.coords.longitude
+		}, () => {
+			log('Shared Location');
+			
+			$shareLocation.removeAttribute('disabled');
+		});
+	});
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 const express = require('express');
 const app = express();
 
@@ -16,8 +17,24 @@ io.on('connection', (socket) => {
    log('New socket connection');
 	const msg = 'Welcome! Start chatting!';
     socket.emit('message', msg);
-	socket.on('sendMessage', (message) => {
+	socket.broadcast.emit('message', 'A new user joined our application');
+	
+	socket.on('sendMessage', (message, cb) => {
+		const filter = new Filter();
+		if(filter.isProfane(message)) {
+			return cb('You cannot use bad words in your message');
+		}
 		io.emit('message', message);
+		cb();
+	});
+	socket.on('sendLocation', (coords, cb) => {
+			io.emit('message', `https://www.google.com/maps?q=${coords.lat},${coords.long}`);
+			cb();
+
+	});
+	
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left our application')	
 	});
 });
 
